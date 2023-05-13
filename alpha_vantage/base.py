45f50fa1ccb12d82ff96c9d19b13af069c5e0587
Max Callé <macalle@protonmail.com>
2023-05-14 00:06:03 +0100

    first attempt with a notebook

--- alpha_vantage/base.py
+++ alpha_vantage/base.py
@@ -7,6 +7,9 @@ class alpha_vantage_base:
         self._API_KEY =  environ.get("AV_API_KEY")
         pass
 
-    def time_series(self, ticker : str) -> str:
-        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={ticker}&apikey={self._API_KEY}'
+    def time_series(self, 
+                    ticker : str,
+                    output_size : str = "full",
+                    data_type :str = "json") -> str:
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={ticker}&outputsize={output_size}&apikey={self._API_KEY}'
         return requests.get(url)
