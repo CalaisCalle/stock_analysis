e2265b00d56f3f87002a8ef285d0c484cd8f1b16
Max Callé <macalle@protonmail.com>
2023-05-13 15:41:54 +0100

    Added some initial files

---
+++ alpha_vantage/base.py
@@ -0,0 +1,6 @@
+import requests
+
+class alpha_vantage_base:
+
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,8 @@
+'''Main: Analyses some stocks or something idk'''
+import alpha_vantage
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
